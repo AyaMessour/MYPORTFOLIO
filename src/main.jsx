0681ad58bc8176528c951df0d6bba180950af51f
Aya Messour <ayamessour35@gmail.com>
2025-12-20 20:25:11 +0000

    Initial commit: Ultimate Task Dashboard portfolio project

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ContactForm from './ContactForm.jsx'
-import Navbar from './navbar.jsx'
+
+import Navbar from './Navbar.jsx'  // ✅ Correct (capital 'N')
 import Hero from './Hero-section.jsx'
 import About from './About.jsx'
 import Skills from './Skills.jsx'
